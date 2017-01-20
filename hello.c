ffa733779b9fb70d65a84d491cf05b0ba34c9931
naveensadanand <naveen.sadanand@gmail.com>
2017-01-20 22:53:56 +0530

    checking auto commit in jenkins remote repo

--- hello.c
+++ hello.c
@@ -2,8 +2,10 @@
 #include "helloworld.h"
 int main() {
     printf("Hello\n");
-    a=5;
-    b=5;
+    int a=5;
+    int b=5;
     printf("sum=%d",a+b);
+    int c=10;
+    printf("c=%d\n", c);
      
 }
